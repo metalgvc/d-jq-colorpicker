86b0f86a87206c4c3c6549394f5f97d0e09d0862
Oleg Gatseluk <metal.gvc@gmail.com>
2013-05-20 18:19:21 +0300

    -

--- jquery.dpicker.js
+++ jquery.dpicker.js
@@ -8,22 +8,42 @@
 			
 			showInfoPanel: true,	// show color info panel
 			
-			panel: '<div id="djqcpPanel" style="border: 1px solid #999; background-color:#cccccc; position: fixed; top:5px; right: 5px; width:130px; height:25px;"><div id="djqcpOutColor" style="float:left; width:25px; height:25px;"></div><div id="djqcpOutText" style="float:left;"></div><div id="djqcpClose" style="padding-right:2px; float:right;"><b>x</b></div></div>'
+			panel: '<div id="djqcpPanel" style="border: 1px solid #999; background-color:#cccccc; position: fixed; top:5px; right: 5px; width:130px; height:25px;cursor:default !important;"><div id="djqcpOutColor" style="float:left; width:25px; height:25px;cursor:default !important;"></div><input id="djqcpOutText" style="float:left;cursor:text !important;" value="" size="7" /><div id="djqcpClose" style="padding-right:2px; float:right;cursor:default !important;"><a href="#" style="text-decoration:none;cursor:pointer !important;"><b style="cursor:pointer !important;">x</b></a></div></div>',
+
+			css: '.jdpicker-cursor, .jdpicker-cursor * {cursor:crosshair;}'
 		},
-		
+
+		const: {
+			STATUS_COLOR_NOT_SELECTED: 1,
+			STATUS_COLOR_SELECTED: 2
+		},
+
 		_lastColor: '',
-		
+		_mainObj: null,
+
+		_status: 1,
+
 		init: function(opt){
 			$.extend(plugin.config, opt);
-			
+
+			plugin._mainObj = this;
+
+			if($('style#jdpickerCSS').length == 0){
+				$('<style>')
+					.attr({id: 'jdpickerCSS', type: 'text/css'})
+					.html(plugin.config.css)
+					.appendTo('head')
+				;
+			}
+
 			if((typeof plugin.config.onSelectColor == 'function'
 				|| typeof plugin.config.onMouseMove == 'function'
 				) && !opt.showInfoPanel
 			){
 				plugin.config.showInfoPanel = false;
 			}
-			
-			return this.bind('click.dpicker', function(){
+
+			return plugin._mainObj.bind('click.dpicker', function(){
 				
 				if(plugin.isActivated()){
 					plugin.deactivate();
@@ -37,45 +57,59 @@
 		// bind click, mousemove events and show color panel
 		activate: function(){
 		
-			if(plugin.isActivated()){ return; }
-			
-			$(document).bind('mousemove.dpicker', function(e){
+			if(plugin.isActivated()){ return false; }
+
+			$('html').addClass('jdpicker-cursor');
+
+			// onMouseMove callback
+			$(document).unbind('mousemove.dpicker').bind('mousemove.dpicker', function(e){
 				if(typeof plugin.config.onMouseMove == 'function'){
 					plugin.config.onMouseMove(e);
 				}else{
 					plugin.onMouseMove(e);
 				}
 			});
-			
-			$(document).bind('click.dpicker', function(e){
+
+			// select color
+			$(document).unbind('click.dpicker').bind('click.dpicker', function(e){
+
 				e.preventDefault();
 				e.stopPropagation();
-				
+
+				// select color on all elements except 'infoPanel' and 'activation' element
+				if($(e.target).is('#'+ plugin._mainObj.attr('id') +',#'+ plugin._mainObj.attr('id') +' *')
+					|| $(e.target).is('div#djqcpPanel, div#djqcpPanel *')
+				){
+					return;
+				}
+
 				if(typeof plugin.config.onSelectColor == 'function'){
 					plugin.config.onSelectColor(e);
 				}else{
+					plugin._status = (plugin._status == plugin.const.STATUS_COLOR_SELECTED)
+						? plugin.const.STATUS_COLOR_NOT_SELECTED : plugin.const.STATUS_COLOR_SELECTED;
 					plugin.onSelectColor(e);
 				}
 			});
-			
-			$('body, a').css('cursor', 'crosshair');
-			
+
 			plugin.showPanel();
+
+			plugin._status = plugin.const.STATUS_COLOR_NOT_SELECTED;
 		},
 		
 		// unbind plugin events and remove color panel
 		deactivate: function(){
-			$(document).unbind('.dpicker');
+			$(document).not(plugin._mainObj).unbind('.dpicker');
+			$('html').removeClass('jdpicker-cursor');
 			plugin.hidePanel();
+			plugin._status = plugin.const.STATUS_COLOR_NOT_SELECTED;
+
 		},
 		
 		// click event on target color element
 		onSelectColor: function(e){
-			$('body, a').css('cursor', '');
-			//$(document).unbind('mousemove.dpicker');
-			
 			if(plugin._lastColor == ''){
-				plugin._lastColor = plugin.getColor(e.target);
+				plugin._lastColor = plugin.getColor(e);
 			}
 			
 			plugin.setPanelInfo(plugin._lastColor);
@@ -83,50 +117,60 @@
 		
 		// mousemove event on target color element
 		onMouseMove: function(e){
-			plugin._lastColor = plugin.getColor(e.target);
+			if(plugin._status == plugin.const.STATUS_COLOR_SELECTED){ return false; }
+			plugin._lastColor = plugin.getColor(e);
 			plugin.setPanelInfo(plugin._lastColor);
 		},
-		
+
 		// check if dpicker is activated
 		isActivated: function(){
-			if($('#djqcpPanel').length){
+			if($('div#djqcpPanel').length){
 				return true;
 			}else{
 				return false;
 			}
 		},
-		
+
 		// show color info panel
 		showPanel: function(){
 			if(!plugin.config.showInfoPanel){
 				return false;
 			}
 			
-			$('body').append($(plugin.config.panel));
+			$('body').append(plugin.config.panel);
 			$('#djqcpClose').bind('click.dpicker', function(){
 				plugin.deactivate();
+				plugin.hidePanel();
 			});
+
+			$('input#djqcpOutText')
+				.unbind('keyup.dpicker')
+				.bind('keyup.dpicker', function(){
+					$('#djqcpOutColor').css('background-color', $(this).val());
+				});
 		},
 		
 		// remove hide info panel
 		hidePanel: function(){
 			$('#djqcpPanel').remove();
 		},
-		
+
+		// fill panel info
 		setPanelInfo: function(color){
-			$('#djqcpOutText').text(color);
+			$('#djqcpOutText').val(color);
 			$('#djqcpOutColor').css('background-color', color);
 		},
 		
 		// get color from html object and convert to #hex
-		getColor: function(obj){
+		getColor: function(event){
 			var color = false;
-			if($(obj).prop('tagName').toLowerCase() == 'img'){
-				color = plugin._getImgColor(obj);
+			if($(event.target).prop('tagName').toLowerCase() == 'img'){
+				color = plugin._getImgColor(event);
 			}else{
-				color = plugin._getTextElementColor(obj);
+				color = plugin._getTextElementColor(event.target);
 			}
-			
+
+			var rgb;
 			if(	typeof color == 'string' 
 				&& (rgb = color.match(/rgb(a)?\(([0-9]+)\,\s?([0-9]+)\,\s?([0-9]+)(,\s?[0-9]+)?\)/))
 			){
@@ -137,8 +181,33 @@
 		},
 		
 		// get current pixel color from img
-		_getImgColor: function(obj){
-			// TODO
+		_getImgColor: function(e){
+			var color = '';
+			try {
+				if(!e.target.canvas) {
+					var canvas = document.createElement('canvas');
+					canvas.width = e.target.width;
+					canvas.height = e.target.height;
+					e.target.canvas = canvas;
+					e.target.canvas.getContext('2d').drawImage(e.target, 0, 0, e.target.width, e.target.height);
+				}
+
+				var offsetX, offsetY;
+				if(typeof e.offsetX !== 'undefined'){
+					offsetX = e.offsetX;
+					offsetY = e.offsetY;
+				}else{
+					offsetX = e.pageX - $(e.target.canvas).offset().left;
+					offsetY = e.pageY - $(e.target.canvas).offset().top;
+				}
+
+				var pixelData = e.target.canvas.getContext('2d').getImageData(offsetX, offsetY, 1, 1).data;
+				color = plugin._rgb2hex(pixelData[0], pixelData[1], pixelData[2]);
+			} catch(err){
+				$.error(err.message);
+			}
+
+			return color;
 		},
 		
 		// TODO:
@@ -173,9 +242,7 @@
 			return '#'+ r + g + b;
 		}
 	};
-	
-	var self = plugin;
-	
+
 	$.fn.dpicker = function( method ){
 		
 		if ( plugin[method] ) {
@@ -186,8 +253,10 @@
 			$.error( 'Method not found' );
 		}
 		
+	};
+
+	if(typeof $.dpicker === 'undefined'){
+		$.dpicker = $.fn.dpicker;
 	}
-	
-	$.dpicker = $.fn.dpicker;
 	
 })(jQuery);
